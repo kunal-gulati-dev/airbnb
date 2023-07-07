75d668a73773aa1b7acfa4873e73abb4dd7d5b6e
kunal-gulati-dev <knlgulati365@gmail.com>
2023-07-07 10:11:05 +0530

    added the project

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import Modal from './components/modals/Modal';
+import Navbar from './components/navbar/Navbar';
+import './globals.css'
+import { Nunito } from 'next/font/google'
+
+const font = Nunito({ subsets: ["latin"] });
+
+export const metadata = {
+  title: 'Airbnb',
+  description: 'Airbnb clone',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+		<html lang="en">
+			<body className={font.className}>
+        <Modal isOpen />
+        <Navbar />
+        {children}
+      </body>
+		</html>
+  );
+}
