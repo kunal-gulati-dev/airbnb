4f9b149a72135fd4082e61069540c02a2d8124bc
kunal-gulati-dev <knlgulati365@gmail.com>
2023-07-07 12:59:53 +0530

    added the prisma configuration

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -4,9 +4,11 @@ import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
 import { useCallback, useState } from "react";
 import MenuItem from "./MenuItem";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
 
 const UserMenu = () => {
 
+    const registerModal = useRegisterModal();
     const [isOpen, setIsOpen] = useState(false)
 
     const toggleOpen = useCallback(() => {
@@ -31,7 +33,7 @@ const UserMenu = () => {
                     <div className="flex flex-col cursor-pointer">
                         <>
                             <MenuItem onclick={() => {}} label="Login" />
-                            <MenuItem onclick={() => {}} label="Sign up" />
+                            <MenuItem onclick={registerModal.onOpen} label="Sign up" />
                         </>
                     </div>
                 </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import Modal from './components/modals/Modal';
+import RegisterModal from './components/modals/RegisterModal';
 import Navbar from './components/navbar/Navbar';
 import './globals.css'
 import { Nunito } from 'next/font/google'
+import ToasterProvider from './providers/ToasterProvider';
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -18,7 +20,8 @@ export default function RootLayout({
   return (
 		<html lang="en">
 			<body className={font.className}>
-        <Modal isOpen />
+        <ToasterProvider />
+        <RegisterModal />
         <Navbar />
         {children}
       </body>
